a25b5558a1c1855620f2b06808eebbdbdf1379be
NightlyD3V <tylerspaulding95@gmail.com>
2025-01-16 18:49:25 -0500

    Initial project set up

---
+++ index.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for port 465, false for other ports
+  auth: {
+    user: "TylerSpaulding95@gmail.com",
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <TylerSpaulding95@gmail.com>', // sender address
+    to: "Tylerspaulding95@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
