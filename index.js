e1a6660c9154385d411cf53f53258b6401243e48
NightlyD3V <tylerspaulding95@gmail.com>
2025-01-16 23:55:15 -0500

    Successful  post request to send an email

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import nodemailer from "nodemailer";
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
+
+const app = express();
+const corsOptions = {
+    origin: 'http://localhost:5500',  // Allow frontend from localhost:3000
+  };
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,26 +22,30 @@ const transporter = nodemailer.createTransport({
       pass: process.env.EMAIL_PASS,
     },
   });
+  async function main(req,res) {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+    from: `<${req.body.email}>`, // sender address
+    to: "Tylerspaulding95@gmail.com", // list of receivers
+    subject: req.body.name, // Subject line
+    text: req.body.message, // plain text body
+    });
 
-const app = express();
-app.listen(3000, () => console.log("Listening on port 3000"));
+    console.log("Message sent: %s", info.messageId);
+    res.status(200).send("Message sent: %s", info.messageId);
+}
 
-app.get("/", (req, res) => {
+app.post("/email", async (req, res) => {
+    console.log("endpoint hit", req.body);
     res.send("Attempting to send message...");
         // async..await is not allowed in global scope, must use a wrapper
-    async function main(req) {
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-        from: `<${req.email}>`, // sender address
-        to: "Tylerspaulding95@gmail.com", // list of receivers
-        subject: req.subject, // Subject line
-        text: req.text, // plain text body
-        });
-    
-        console.log("Message sent: %s", info.messageId);
-        res.send("Message sent: %s", info.messageId);
-    }
-    
-    main().catch(console.error);
+        try {
+            main(req, res).catch(console.error);
+            res.status(200).res.send("Message sent");
+        } catch (error) {
+            console.log(error);
+        }
 })
+
+app.listen(3000, () => console.log("Listening on port 3000"));
 
