6158fe1c5f307d8641cd66998e6a7e5ad7537d70
NightlyD3V <tylerspaulding95@gmail.com>
2025-01-17 13:05:21 -0500

    fixing from address

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for port 465, false for other ports
-    auth: {
-      user: "TylerSpaulding95@gmail.com",
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-  async function main(req,res) {
+  async function main(req) {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user: req.body.email,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
     // send mail with defined transport0 object
     const info = await transporter.sendMail({
     from: req.body.email, // sender address
@@ -37,7 +37,7 @@ app.post("/email", async (req, res) => {
     console.log("endpoint hit", req.body);
         // async..await is not allowed in global scope, must use a wrapper
         try {
-            const result = await main(req, res).catch(console.error)
+            const result = await main(req).catch(console.error)
             res.status(200).send("Message sent! 🥰")
         } catch (error) {
             console.log(error);
